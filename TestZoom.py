b0c1102dbfc50bec7e760648ffdecfb51dcdf9f1
BraydentheCao <bcao89933@gmail.com>
2026-08-10 00:15:58 -0500

    Made test zoom to see if camera zoom can work (The camera had to be moved futher back)

--- TestZoom.py
+++ TestZoom.py
@@ -7,40 +7,28 @@ app = Flask(__name__)
 
 # Initialize Picamera2
 picam2 = Picamera2()
-config = picam2.create_preview_configuration(main={"size": (800, 600)})
+
+config = picam2.create_preview_configuration(main={"size": (1200, 1200)})
 picam2.configure(config)
 picam2.start()
+
+full_w,full_h = picam2.sensor_resolution
+zoom_factor_x = 3.0
+zoom_factor_y = 3.5
+crop_w = int(full_w/zoom_factor_x)
+crop_h = int(full_h/zoom_factor_y)
+crop_xInit = int(full_w - crop_w)//2
+crop_yInit = int(full_h - crop_h)//2
+
+picam2.set_controls({"ScalerCrop": (crop_xInit, crop_yInit, crop_w, crop_h)})
 
 def generate_frames():
     while True:
         # Get a frame from picam, do initial set up to make sure it can be filtered
         frame = picam2.capture_array()
         frame = cv2.flip(frame,0)
-        frame_bgr = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
-
-        bilateralFilteredFrame = cv2.bilateralFilter(frame_bgr, 9, 75, 75)
-        hsv = cv2.cvtColor(bilateralFilteredFrame, cv2.COLOR_BGR2HSV)
-
-        """
-        the variable hsv is a 3d numpy array that has the same xy dimensions as the image, then is 3 layers thick
-        each layer is a different hsv channel, hue, saturation, and value
-        Each "x,y" coordinate contains each of the 3 hsv values for that pixel
-        it's [row, col, hsv channel] 
-
-        hsv[:,:,0] means every row (: = every row), every column, but only the first channel, hue
         
-        """
-        
-        hsv[:,:,2] = cv2.equalizeHist(hsv[:,:,2])
-
-        # Quantize the brightness channel to create posterization effect
-
-        # v = hsv[]
-
-
-        # histEqualizedFrameBGR = cv2.cvtColor(hsv,cv2.COLOR_HSV2BGR)
-
-        finalFrame = bilateralFilteredFrame
+        finalFrame = frame
 
         #Convert to JPEG for streaming
         ret, buffer = cv2.imencode('.jpg',finalFrame)
